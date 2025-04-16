0904e1e76d871ff5e428e0250013739026c6a63c
takid1412 <duanpq92@gmail.com>
2025-04-16 17:46:29 +0700

    add space after log ts

--- src/log.ts
+++ src/log.ts
@@ -3,16 +3,11 @@ export class Logger {
         let first_parameter = args.shift();
 
         const formatConsoleDate = (date: Date) => {
-            let hour = date.getHours();
-            let minutes = date.getMinutes();
-            let seconds = date.getSeconds();
-            let milliseconds = date.getMilliseconds();
-
             const padding = (num: Number) => {
                 return num.toString().padEnd(2, '0')
             };
 
-            return `[${padding(date.getHours())}:${padding(date.getMinutes())}:${padding(date.getSeconds())}]`;
+            return `[${padding(date.getHours())}:${padding(date.getMinutes())}:${padding(date.getSeconds())}] `;
         }
         console.log.apply(console, [formatConsoleDate(new Date()) + first_parameter].concat(args));
     }
