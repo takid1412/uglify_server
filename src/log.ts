d950988eb7f6d5ee33ca2b7459307e4b55caafab
takid1412 <duanpq92@gmail.com>
2025-04-17 09:42:14 +0700

    show day in log

--- src/log.ts
+++ src/log.ts
@@ -7,7 +7,7 @@ export class Logger {
                 return num.toString().padStart(2, '0')
             };
 
-            return `[${padding(date.getHours())}:${padding(date.getMinutes())}:${padding(date.getSeconds())}] `;
+            return `[${padding(date.getDate())}/${padding(date.getMonth() + 1)}/${date.getFullYear()} ${padding(date.getHours())}:${padding(date.getMinutes())}:${padding(date.getSeconds())}] `;
         }
         console.log.apply(console, [formatConsoleDate(new Date()) + first_parameter].concat(args));
     }
